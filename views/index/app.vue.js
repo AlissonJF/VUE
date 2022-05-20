c6511b0acee573366f77f3f6be0de7b28bee00a8
UNIMAR\ti.estagiario <alissonruan567@gmail.com>
2022-05-20 09:02:34 -0300

    Update

--- views/index/app.vue.js
+++ views/index/app.vue.js
@@ -4,7 +4,7 @@ const AppTemplate = `
     <div align='center'>
         <ejs-chart
         style="display: block"
-        :theme="theme"
+        theme="Material"
         align="center"
         id="chartcontainer"
         :title="title"
@@ -12,7 +12,7 @@ const AppTemplate = `
         :primaryYAxis="primaryYAxis"
         :tooltip="tooltip"
         :chartArea="chartArea"
-        :width="width">
+        width=100%>
             <e-series-collection>
                 <e-series
                 :dataSource="seriesData"
@@ -42,7 +42,59 @@ const AppTemplate = `
 Vue.component('AppVue', {
     template: AppTemplate,
     data: function() {
-        return {}
+        return {
+            seriesData: [
+                { x: new Date(2005, 0, 1), y: 21 },
+                { x: new Date(2006, 0, 1), y: 24 },
+                { x: new Date(2007, 0, 1), y: 36 },
+                { x: new Date(2008, 0, 1), y: 38 },
+                { x: new Date(2009, 0, 1), y: 54 },
+                { x: new Date(2010, 0, 1), y: 57 },
+                { x: new Date(2011, 0, 1), y: 70 }
+            ],
+            seriesData1: [
+                { x: new Date(2005, 0, 1), y: 28 },
+                { x: new Date(2006, 0, 1), y: 44 },
+                { x: new Date(2007, 0, 1), y: 48 },
+                { x: new Date(2008, 0, 1), y: 50 },
+                { x: new Date(2009, 0, 1), y: 66 },
+                { x: new Date(2010, 0, 1), y: 78 },
+                { x: new Date(2011, 0, 1), y: 84 }
+            ],
+            //Initializing Primary X Axis
+            primaryXAxis: {
+                valueType: "DateTime",
+                labelFormat: "y",
+                intervalType: "Years",
+                edgeLabelPlacement: "Shift",
+                majorGridLines: { width: 0 }
+            },
+            //Initializing Primary Y Axis
+            primaryYAxis: {
+                labelFormat: "{value}%",
+                rangePadding: "None",
+                minimum: 0,
+                maximum: 100,
+                interval: 20,
+                lineStyle: { width: 0 },
+                majorTickLines: { width: 0 },
+                minorTickLines: { width: 0 }
+            },
+            chartArea: {
+                border: {
+                    width: 0
+                }
+            },
+            marker: {
+                visible: true,
+                height: 10,
+                width: 10
+            },
+            tooltip: {
+                enable: true
+            },
+            title: "Test Component"
+        }
     },
     methods: {}
 })
